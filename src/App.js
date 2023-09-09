647e1f081b52f030e43685359f5fca7989c99f4f
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2023-09-09 22:50:34 +0100

    rendering list completed

--- src/App.js
+++ src/App.js
@@ -41,5 +41,25 @@ function FriendList() {
 }
 
 function Friend({ friend }) {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          you owe {friend.name} {friend.balance}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
+
+function 
